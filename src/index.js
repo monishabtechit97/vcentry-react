843dcb5d6d29ca592ad4296b3c060c00f2db5e92
monishabtechit97 <monishabtechit97@gmail.com>
2021-03-07 19:50:17 +0530

    initial setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+function App(){
+    return <h1>Hello,Welcome to React JS</h1>
+
+}
+
+ReactDom.render(<App></App>, document.getElementById('root'))
